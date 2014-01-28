1fe03e7ce67b107ad7adedc9032e503a72fba776
Aboobacker MK <aboobackervyd@gmail.com>
2014-01-28 21:19:50 +0530

    added preferences

--- preferences.cpp
+++ preferences.cpp
@@ -7,10 +7,16 @@ Preferences::Preferences(QWidget *parent) :
 {
     ui->setupUi(this);
     LoadSettings();
+    ui->LanguageSelector->setCurrentText(UiLanguage);
+    ui->ThemeSlector->setCurrentText(UiTheme);
+    ui->AmplitudeSlider->setValue(SoundAmplitude);
+    ui->IsSplashScreen->setChecked(IsSplashScreen);
+    ui->ShowAmbiguity->setChecked(IsShowAmbiguity);
 }
 
 Preferences::~Preferences()
 {
+    StoreSettings();
     delete ui;
 }
 
@@ -20,11 +26,61 @@ void Preferences::on_listWidget_currentRowChanged(int currentRow)
 }
 void Preferences::LoadSettings()
 {
-    QSettings settings("Tachyons Creations", "mltranslator");
-   UiLanguage=settings.value("uilanguage").toString();
+    QSettings settings("tachyons", "mltranslator");
+    settings.beginGroup("preferences");
+    UiLanguage=settings.value("uilanguage").toString();
+    UiTheme=settings.value("theme").toString();
+    IsShowAmbiguity=settings.value("ambiguity").toBool();
+    IsSplashScreen=settings.value("splashscreen").toBool();
+    SoundAmplitude=settings.value("soundamplitude").toInt();
+    settings.endGroup();
+    qDebug()<<"Loaded";
+
 }
 void Preferences::StoreSettings()
 {
-    QSettings settings("Tachyons Creations", "mltranslator");
-    UiLanguage=settings.setValue("uilanguage","english");
+    QSettings settings("tachyons", "mltranslator");
+    settings.beginGroup("preferences");
+    settings.setValue("uilanguage",UiLanguage);
+    settings.setValue("theme",UiTheme);
+    settings.setValue("ambiguity",IsShowAmbiguity);
+    settings.setValue("splashscreen",IsSplashScreen);
+    settings.setValue("soundamplitude",SoundAmplitude);
+    settings.endGroup();
+    qDebug()<<"stored";
+}
+
+void Preferences::on_ThemeSlector_currentIndexChanged(const QString &arg1)
+{
+    UiTheme=arg1;
+}
+
+void Preferences::on_IsSplashScreen_toggled(bool checked)
+{
+    IsSplashScreen=checked;
+}
+
+void Preferences::on_LanguageSelector_currentIndexChanged(const QString &arg1)
+{
+    UiLanguage=arg1;
+}
+
+void Preferences::on_AmplitudeSlider_sliderMoved(int position)
+{
+    SoundAmplitude=position;
+}
+
+void Preferences::on_buttonBox_rejected()
+{
+    LoadSettings();
+}
+
+void Preferences::on_buttonBox_accepted()
+{
+    StoreSettings();
+}
+
+void Preferences::on_ShowAmbiguity_toggled(bool checked)
+{
+    IsShowAmbiguity=checked;
 }
--- preferences.h
+++ preferences.h
@@ -3,6 +3,7 @@
 
 #include <QMainWindow>
 #include <QSettings>
+#include<QDebug>
 
 namespace Ui {
 class Preferences;
@@ -18,9 +19,28 @@ public:
     void LoadSettings();
     void StoreSettings();
     QString UiLanguage;
+    QString UiTheme;
+    bool IsSplashScreen;
+    bool IsShowAmbiguity;
+    int SoundAmplitude;
+
     
 private slots:
     void on_listWidget_currentRowChanged(int currentRow);
+
+    void on_ThemeSlector_currentIndexChanged(const QString &arg1);
+
+    void on_IsSplashScreen_toggled(bool checked);
+
+    void on_LanguageSelector_currentIndexChanged(const QString &arg1);
+
+    void on_AmplitudeSlider_sliderMoved(int position);
+
+    void on_buttonBox_rejected();
+
+    void on_buttonBox_accepted();
+
+    void on_ShowAmbiguity_toggled(bool checked);
 
 private:
     Ui::Preferences *ui;
